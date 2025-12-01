70ad79ed9fd34549fca19a595aaaf0e00db17a61
MelinNathan <nathanmelin49@orange.fr>
2025-12-01 09:26:21 +0100

    push grid and jeu

--- grid.py
+++ grid.py
@@ -2,3 +2,16 @@
 # On December 2025
 # Main file to lauch the game
 
+def createGrid(n,m,snake):
+    grid = ''
+    for i in range(n):
+        for j in range(m):
+            if(i,j) in snake:
+                if(i,j) == snake[0]:
+                    print('■',end='')   #print head
+                else:
+                    print('□',end='')   #print body
+            else:
+                print('.',end='')       #print grass
+        print('\n')
+
--- jeu.py
+++ jeu.py
@@ -1,24 +1,6 @@
-"""
-@docstring
-"""
-
-
-def createGrid(n,m,snake):
-    grid = ''
-    for i in range(n):
-        for j in range(m):
-            if(i,j) in snake:
-                if(i,j) == snake[0]:
-                    print('■',end='')   #print head
-                else:
-                    print('□',end='')   #print body
-            else:
-                print('.',end='')       #print grass
-        print('\n')
-
+import grid
 
 if __name__ == "__main__":
-    # grid = createGrid(5,6)
     snake = [(1,0),(2,0),(3,0)]
-    createGrid(10,10,snake)
+    grid.createGrid(10,10,snake)
     
