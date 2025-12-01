c4b5c614cb340708ec5f35a23528b37098455699
MelinNathan <nathanmelin49@orange.fr>
2025-12-01 09:20:42 +0100

    push jeu

--- jeu.py
+++ jeu.py
@@ -1,9 +1,24 @@
-# Made by Nathan M. and Lowan Q.
-# On December 2025
-# Main file to lauch the game
+"""
+@docstring
+"""
 
-def main():
-    return True
 
-if __name__ == '__name__':
-    main()
+def createGrid(n,m,snake):
+    grid = ''
+    for i in range(n):
+        for j in range(m):
+            if(i,j) in snake:
+                if(i,j) == snake[0]:
+                    print('■',end='')   #print head
+                else:
+                    print('□',end='')   #print body
+            else:
+                print('.',end='')       #print grass
+        print('\n')
+
+
+if __name__ == "__main__":
+    # grid = createGrid(5,6)
+    snake = [(1,0),(2,0),(3,0)]
+    createGrid(10,10,snake)
+    
