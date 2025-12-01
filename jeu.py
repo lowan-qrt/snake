d6c5ef8684055c47b7b55d9afc067ecbd9756ddd
Lowan Quarton <lowan.qrt@gmail.com>
2025-12-01 09:16:35 +0100

    First commit

---
+++ jeu.py
@@ -0,0 +1,9 @@
+# Made by Nathan M. and Lowan Q.
+# On December 2025
+# Main file to lauch the game
+
+def main():
+    return True
+
+if __name__ == '__name__':
+    main()
---
+++ snake.py
@@ -0,0 +1,3 @@
+# Made by Nathan M. and Lowan Q.
+# On December 2025
+# Main file to lauch the game
