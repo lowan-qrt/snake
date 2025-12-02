925a6c28a5ac7c96b9f3aa9a682a741fb0fcbe29
MelinNathan <nathanmelin49@orange.fr>
2025-12-02 11:02:41 +0100

    modif grid

--- grid.py
+++ grid.py
@@ -2,12 +2,23 @@
 # On December 2025
 # Main file to lauch the game
 
-def createGrid(n,m,snake):
+def displayGrid(n,m,snake):
+    """
+    Display a grid of n times m units.
+    
+    Args:
+        n (int): number of lines
+        m (int): number of rows
+        snake (list of coordinates (tuples)): coordinates of all part of the snake
+        
+    Returns:
+        None, it just print all element of the game
+    """
     grid = ''
     for i in range(n):
         for j in range(m):
-            if(i,j) in snake:
-                if(i,j) == snake[0]:
+            if(j,i) in snake:
+                if(j,i) == snake[0]:
                     print('■',end='')   #print head
                 else:
                     print('□',end='')   #print body
@@ -15,3 +26,44 @@ def createGrid(n,m,snake):
                 print('.',end='')       #print grass
         print('\n')
 
+def displayCircle(r,snake):
+    """
+    Display the grid as a circle
+    
+    Args:
+        r (int) : radius (min : 5) 
+    """
+    
+    n = 2 * r + 1
+    for i in range(n ):
+        for j in range(n):
+          
+       
+            dx = (j ) - r
+            dy = (i ) - r
+            distance = (dx**2 + dy**2)**0.5
+            if distance <= r - 0.5:
+                print('.', end='')
+            else:
+                print('▓', end='')
+        print('\n')
+    
+    # for i in range(r*2+3):
+        # for j in range(r*2+3):
+            # if(i,j) in snake:
+                # if(i,j) == snake[0]:
+                    # print('■',end='')   #print head
+                # else:
+                    # print('□',end='')   #print body
+            # else:
+                
+                # if ((i <= 2 and j <=1) or (i <= 1 and j <=2) or (i <= 2 and j >=r*2+1) or (i <= 1 and j >=r*2) or (i >= r*2 and j <=1) or (i >= r*2+1 and j <=2) or  (i >= r*2 and j >=r*2+1) or (i >= r*2+1 and j >=r*2) or (i == 0) or (j == 0) or (i == 2*r+2) or (j == 2*r+2)):
+                # if ((i,j) in grassZone):    
+                    # print('.',end='') #print grass                   
+                # else:
+                    # print('▓',end='')       #print wall
+        # print('\n')
+
+    
+# def maze(m,n, snake):   #Si on a le temps car ça à l'air galère à faire, mais fun
+    
--- jeu.py
+++ jeu.py
@@ -3,4 +3,4 @@ import grid
 if __name__ == "__main__":
     snake = [(1,0),(2,0),(3,0)]
     grid.createGrid(10,10,snake)
-    
+    print(type(snake))
