8926b130d42a80e40b3c7357adae9814a5c10849
MelinNathan <nathanmelin49@orange.fr>
2025-12-02 11:29:26 +0100

    fix movement

--- snake.py
+++ snake.py
@@ -60,25 +60,25 @@ class Snake:
                     # Up
                     self.coordinates.insert(
                         0,
-                        (self.coordinates[0][0] - 1, self.coordinates[0][1])
+                        (self.coordinates[0][0], self.coordinates[0][1] - 1)
                     )
                 case 'Q':
                     # Left
                     self.coordinates.insert(
                         0,
-                        (self.coordinates[0][0], self.coordinates[0][1] - 1)
+                        (self.coordinates[0][0] - 1, self.coordinates[0][1])
                     )
                 case 'S':
                     # Down
                     self.coordinates.insert(
                         0,
-                        (self.coordinates[0][0] + 1, self.coordinates[0][1])
+                        (self.coordinates[0][0], self.coordinates[0][1]  + 1)
                     )
                 case 'D':
                     # Right
                     self.coordinates.insert(
                         0,
-                        (self.coordinates[0][0], self.coordinates[0][1] + 1)
+                        (self.coordinates[0][0] + 1, self.coordinates[0][1])
                     )
                 case _:
                     self.coordinates.insert(1, last_tail)
